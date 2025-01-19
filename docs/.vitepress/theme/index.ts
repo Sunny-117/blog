d9f02e6a4ca05212a7b18e1480431e655b8aa2c3
Sunny-117 <zhiqiangfu6@gmail.com>
2025-01-19 11:12:47 +0800

    feat: add oh-my-live2d

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,46 @@
 import Theme from "vitepress/theme";
 import "./style/var.css";
+import { EnhanceAppContext } from "vitepress";
 
 export default {
   ...Theme,
+  async enhanceApp({ app, router }: EnhanceAppContext) {
+    const { loadOml2d } = await import("oh-my-live2d");
+    loadOml2d({
+      models: [
+        {
+          path: "https://note.bingkele.cc/models/Kar98k-normal/model.json",
+          position: [0, 60],
+          scale: 0.08,
+          stageStyle: {
+            height: 450,
+          },
+        },
+        {
+          path: "https://note.bingkele.cc/models/HK416-1-normal/model.json",
+          position: [0, 60],
+          scale: 0.08,
+          stageStyle: {
+            height: 450,
+          },
+        },
+        {
+          path: "https://note.bingkele.cc/models/HK416-2-destroy/model.json",
+          position: [0, 60],
+          scale: 0.08,
+          stageStyle: {
+            height: 450,
+          },
+        },
+        {
+          path: "https://note.bingkele.cc/models/HK416-2-normal/model.json",
+          position: [0, 60],
+          scale: 0.08,
+          stageStyle: {
+            height: 450,
+          },
+        },
+      ],
+    });
+  },
 };
