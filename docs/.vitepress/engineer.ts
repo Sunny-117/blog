b1eb986489a9cdd21d7f2429a07d46dc0fa2a9e1
Sunny-117 <zhiqiangfu6@gmail.com>
2025-06-04 17:43:04 +0800

    remove eng

--- docs/.vitepress/engineer.ts
+++ docs/.vitepress/engineer.ts
@@ -1,63 +1,7 @@
 export default [
   {
-    text: "前端工程化 OnePage",
-    link: "/front-end-engineering/engineering-onepage",
-  },
-  {
-    text: "Npm package：npm发包必知必会",
-    link: "https://juejin.cn/post/7093873173128544270",
-  },
-  {
-    text: "中高级前端必须掌握的package.json最新最全指南",
-    link: "https://juejin.cn/post/7240805459288522808",
-  },
-  {
-    text: "前端性能优化方法论",
-    link: "/front-end-engineering/performance",
-  },
-  {
-    text: "webpack常用拓展",
-    link: "/front-end-engineering/webpack常用拓展",
-  },
-  {
-    text: "CSS工程化",
-    link: "/front-end-engineering/CSS工程化",
-  },
-  {
-    text: "CSS 预处理器之SCSS",
-    link: "/front-end-engineering/CSS 预处理器之SCSS",
-  },
-  {
-    text: "CSS 后处理器之PostCSS",
-    link: "/front-end-engineering/后处理器",
-  },
-  {
-    text: "JS 兼容性",
-    link: "/front-end-engineering/jscompatibility",
-  },
-  {
-    text: "webpack5 模块联邦",
-    link: "/front-end-engineering/webpack5-mf",
-  },
-  {
-    text: "pnpm原理",
-    link: "/front-end-engineering/pnpm原理",
-  },
-  {
-    text: "模块化",
-    link: "/front-end-engineering/modularization",
-  },
-  {
-    text: "包管理器",
-    link: "/front-end-engineering/PackageManager",
-  },
-  {
     text: "主题切换",
     link: "/front-end-engineering/theme",
-  },
-  {
-    text: "纯原生开发 Web Components 超详细分享",
-    link: "https://juejin.cn/post/7158717654369304612",
   },
   {
     text: "Node 组成原理",
